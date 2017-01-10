fc962e559ee73eac802f51b9a6b2a3922a3233cb
Xiwen Cheng <xiwen.cheng@mendix.com>
2017-01-10 17:14:05 +0100

    Render graph once due to performance issues

--- src/TimeSeries/widget/TimeSeries.js
+++ src/TimeSeries/widget/TimeSeries.js
@@ -53,6 +53,7 @@ define([
 
         // Parameters configured in the Modeler.
         graphSourceURL: "",
+        isRendered: false,
 
         // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
         _handles: null,
@@ -175,6 +176,10 @@ define([
 
         // Helper/Internal functions
         _renderGraph: function () {
+          if (this.isRendered) {
+            return;
+          }
+          this.isRendered = true;
           var widget = this;
           this._contextObj.fetch(this.graphSourceURL, function(url) {
               d3.json(url, function(data) {

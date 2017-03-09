2966d72f072def32c910e3aca6641d06ac3b6bf3
Emir Ozer <emirozer@yandex.com>
2017-03-09 16:18:26 +0100

    fixate min and max vals for line charts to solve rendering bug

--- src/TimeSeries/widget/TimeSeries.js
+++ src/TimeSeries/widget/TimeSeries.js
@@ -238,8 +238,13 @@ define([
 
           nv.addGraph(function() {
             var chart;
+            var maxValCandidates = [];
             if (graphData.render == "line") {
               chart = nv.models.lineChart();
+              for (var i = 0; i < data.length; i++) {
+                maxValCandidates.push(Math.max( ...data[i].values ));
+              }
+                chart.lines.forceY([0.0, Math.max( ...maxValCandidates )]);
             } else {
               chart = nv.models.stackedAreaChart();
               chart = chart.showControls(false);
